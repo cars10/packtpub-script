5da674827b3741297dfdd4da7f39cfce8dbf4e7e
Carsten König <carstenkoenig92@gmail.com>
2016-06-23 14:07:41 +0200

    initial

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+# ruby-2.2.3
+
+gem 'nokogiri', '~> 1.6.8'
+gem 'watir-webdriver', '~> 0.9.1'
+gem 'webdriver-user-agent'
+gem 'phantomjs', '~> 2.1.1.0'
---
+++ script.rb
@@ -0,0 +1,52 @@
+#== Script to automatically add free packtpublishing book to your library ==#
+# Setup
+# ## create .ruby-version file and add the ruby version, for example ruby-2.3.0
+# ## create .ruby-gemset file and add a gemset, for example packt-script
+# ## run `bundle install`
+# run the script with: "ruby script.rb"
+
+## change these to fit your packtpub account
+#== config ==#
+your_email = "CHANGEME"
+your_password = "CHANGEME"
+
+
+#== require gems ==#
+require 'rubygems' # gems
+require 'open-uri' # uri tools
+require 'watir-webdriver' # open & interact with browser
+require 'webdriver-user-agent'
+require 'phantomjs' # headless browser
+require 'net/smtp' # email
+
+#== variables ==#
+button_class = "twelve-days-claim"
+base_url = "https://www.packtpub.com/"
+free_learning_url = "https://www.packtpub.com/packt/offers/free-learning"
+
+# create browser
+browser = Watir::Browser.new :phantomjs # open new browser
+browser.window.resize_to(1920, 1080) # go full width because we are mobile otherwise
+
+# login
+browser.goto base_url # go to url
+browser.a(class: 'login-popup').when_present(5).click # wait till page is loaded
+browser.form(id: 'packt-user-login-form').text_field(name: 'email').set(your_email)
+browser.form(id: 'packt-user-login-form').text_field(name: 'password').set(your_password)
+browser.form(id: 'packt-user-login-form').button(type: 'submit').click
+browser.div(id: 'account-bar-logged-in').wait_until_present(5) # user is logged in
+
+# get free book
+browser.goto free_learning_url # go to url
+book_title = browser.div(class: 'dotd-title').when_present(5).h2.text
+browser.a(class: 'twelve-days-claim').when_present(5).click
+#browser.screenshot.save 'screenshot.png'
+browser.close
+
+#== email ==#
+message = "Added new book: #{book_title}"
+
+Net::SMTP.start('localhost') do |smtp|
+  smtp.send_message message, 'root@cars10k.de',
+                             'carstenkoenig92@gmail.com'
+end
